77c8d33b9726a83f25b49e0516e7a58569dbd879
leedwon <leedwon12@gmail.com>
2019-01-21 14:45:52 +0100

    update Converter so it can now create Image and Ox objects, create Ox ctor, reformat code with ReSharper

--- GraphicProject/Converter.cpp
+++ GraphicProject/Converter.cpp
@@ -1,70 +1,79 @@
 #include "Converter.h"
 
 
-
-Converter::Converter()
-{
+Converter::Converter() {
 }
 
-void Converter::createPalette(Ox *ox)
-{
-	for(int i = 0; i < Constants::PALETTE_SIZE; ++i)
-	{
-		ox->colorPalette[i] = bits_to_sdl_color(i);
+// just temporary solution pallete should be created based on image TODO :: 
+std::array<SDL_Color, Constants::PALETTE_SIZE> Converter::createDedicatedPalette(Image* img) {
+	std::array<SDL_Color, Constants::PALETTE_SIZE> result;
+	return result;
+}
+
+std::vector<std::vector<Constants::oxColor>> Converter::createOxPixelsFromImage(Image* image) {
+	std::vector<std::vector<Constants::oxColor>> pixels;
+	pixels.resize(image->getWidth()); // init vector
+	for (int i = 0; i < image->getWidth(); ++i) {
+		pixels[i].resize(image->getHeight()); // init vector
+	}
+	for (int y = 0; y < image->getHeight(); ++y) {
+
+		for (int x = 0; x < image->getWidth(); ++x) {
+			pixels[x][y] = SdlColor_to_oxPixel(image->getPixel(x, y));
+		}
+	}
+	return pixels;
+}
+
+std::vector<std::vector<SDL_Color>> Converter::createSdlPixelsFromOx(Ox* ox) {
+	std::vector<std::vector<SDL_Color>> pixels;
+	pixels.resize(ox->getWidth()); // init vector
+	for (int i = 0; i < ox->getWidth(); ++i) {
+		pixels[i].resize(ox->getHeight()); // init vector
+	}
+	for (int y = 0; y < ox->getHeight(); ++y) {
+		for (int x = 0; x < ox->getWidth(); ++x) {
+			pixels[x][y] = oxPixel_to_sdlColor(ox->getPixel(x, y));
+		}
 	}
 }
 
 
-
-uint8_t Converter::reduceTo6Bits(SDL_Color color)
-{
+uint8_t Converter::SdlColor_to_oxPixel(SDL_Color color) {
 	uint8_t oxColor = 0;
-	oxColor |= (color.r & Constants::MASK_6_BITS) << 4;
-	oxColor |= (color.g & Constants::MASK_6_BITS) << 2;
-	oxColor |= (color.b & Constants::MASK_6_BITS);
+	oxColor |= (color.r & Constants::MASK_FIRST_3_BITS) << 5;
+	oxColor |= (color.g & Constants::MASK_FIRST_3_BITS) << 2;
+	oxColor |= (color.b & Constants::MASK_FIRST_2_BITS);
 	return oxColor;
 }
 
-SDL_Color Converter::bits_to_sdl_color(uint8_t oxColor)
-{
+
+SDL_Color Converter::oxPixel_to_sdlColor(Constants::oxColor oxColor) {
 	SDL_Color color;
-	color.r = oxColor & Constants::MASK_6_BITS_RED;
-	color.g = oxColor & Constants::MASK_6_BITS_GREEN;
-	color.b = oxColor & Constants::MASK_6_BITS_BLUE;
+	color.r = oxColor & Constants::MASK_BITS_RED;
+	color.g = oxColor & Constants::MASK_BITS_GREEN;
+	color.b = oxColor & Constants::MASK_BITS_BLUE;
 	color.a = 1;
 	return color;
 }
 
 
-void Converter::convertImageToOxShiftBits(Image* image, Ox* ox)
-{
-	ox->width = image->getWidth();
-	ox->height = image->getHeight();
-	for(int y = 0; y < image->getHeight(); ++y)
-	{
-
-		for (int x = 0; x < image->getWidth(); ++x)
-		{
-			ox->pixels[x][y] = reduceTo6Bits(image->getPixel(x, y));
-		}
-	}
-	createPalette(ox);
-	ox->paletteType = Constants::PALETTE_TYPE_BYTE_SHIFT;
+Ox Converter::convertImageToOx(Image* image) {
+	int width = image->getWidth();
+	int height = image->getHeight();
+	int paletteType = 0; // refactor later
+	std::vector<std::vector<Constants::oxColor>> pixels = createOxPixelsFromImage(image);
+	std::array<SDL_Color, Constants::PALETTE_SIZE> palette = createDedicatedPalette(image);
+	return Ox(paletteType, width, height, pixels, palette);
 }
 
-void Converter::convertOxToImage(Image* image, Ox* ox)
-{
-	image->setImageDimensions(ox->width, ox->height);
-	for(int y = 0; y < ox->height; ++y)
-	{
-		for (int x = 0; x < ox->width; ++x)
-		{
-			image->setPixel(x, y, ox->getSdlColor(x, y));
-		}	
-	}
+Image Converter::convertOxToImage(Ox* ox) {
+	std::vector<std::vector<SDL_Color>> pixels = createSdlPixelsFromOx(ox);
+	int width = ox->getWidth();
+	int height = ox->getHeight();
+	return Image(width, height, pixels);
 }
 
 
-Converter::~Converter()
-{
+Converter::~Converter() {
 }
--- GraphicProject/Image.cpp
+++ GraphicProject/Image.cpp
@@ -2,8 +2,7 @@
 #include "BmpFileReader.h"
 
 
-void Image::initVectors(int width, int height)
-{
+void Image::initVectors(int width, int height) {
 	pixelMap.resize(width); // init vectors
 	for (int i = 0; i < width; ++i) {
 		pixelMap[i].resize(height); // init vectors
@@ -17,46 +16,35 @@ Image::Image(std::string filePath) {
 	this->height = fileReader.getHeight();
 	this->width = fileReader.getWidth();
 	initVectors(width, height);
-	for(int i = 0; i < width; ++i) {
+	for (int i = 0; i < width; ++i) {
 		for (int j = 0; j < height; j++)
 			pixelMap[i][j] = fileReader.getPixel(i, j);
 	}
 }
 
-Image::Image(int width, int height)
-{
-	setImageDimensions(width, height);
-}
+Image::Image(int w, int h,
+             std::vector<std::vector<SDL_Color>> pixelVector) : width(w), height(h), pixelMap(pixelVector) {
 
-void Image::setImageDimensions(int width, int height)
-{
-	this->width = width;
-	this->height = height;
-	initVectors(width, height);
 }
 
 
-int Image::getWidth()
-{
+int Image::getWidth() {
 	return width;
 }
 
-int Image::getHeight()
-{
+int Image::getHeight() {
 	return height;
 }
 
 
-Image::~Image(){
+Image::~Image() {
 
 }
 
-SDL_Color Image::getPixel(int x, int y)
-{
+SDL_Color Image::getPixel(int x, int y) {
 	return pixelMap[x][y];
 }
 
-void Image::setPixel(int x, int y,SDL_Color color)
-{
+void Image::setPixel(int x, int y, SDL_Color color) {
 	pixelMap[x][y] = color;
 }
