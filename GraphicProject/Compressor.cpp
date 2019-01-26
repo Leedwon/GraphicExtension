2a90c1f2a7a6216556b3a4cb984d89cdf3e3e2a3
Leedwon <leedwon12@gmail.com>
2019-01-26 20:52:06 +0100

    resolve double compress method conflict

--- GraphicProject/Compressor.cpp
+++ GraphicProject/Compressor.cpp
@@ -21,13 +21,6 @@ bool Compressor::areThreeInRowSame(const std::vector<std::vector<Constants::oxPi
 	return true;
 }
 
-//To Do
-std::vector<uint8_t> Compressor::compress(const std::vector<std::vector<Constants::oxPixel>>& pixels)
-{
-	return Compressor::compressByteRun(pixels); 
-}
-
-
 void Compressor::addSequenceOfDifferentSymbols(std::vector<uint8_t> &buffer, std::vector<uint8_t> &destination) {
 	destination.push_back(0); // inform that we will be copying next buffer.size() amount of bits
 	destination.push_back(static_cast<uint8_t>(buffer.size())); // how many to copy
