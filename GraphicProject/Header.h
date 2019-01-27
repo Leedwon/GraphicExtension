737831d5a1da55f8daf20d37035eefed7047259d
Hubert <hubert15nowak@gmail.com>
2019-01-27 12:55:15 +0100

    add enum conversion to int

--- GraphicProject/Header.h
+++ GraphicProject/Header.h
@@ -16,12 +16,13 @@ private:
 		uint32_t width;
 		uint32_t height;
 		uint8_t paletteType;
-		uint8_t dithering;
 		uint8_t compressionType;
 	};
 public:
 	FileHeader fileHeader{};
 	PictureHeader pictureHeader{};
+	uint8_t static paletteTypeToInt(Constants::paletteType palette);
+	Constants::paletteType static intToPaletteType(uint8_t palette);
 	Header(Ox* ox, uint32_t sizeOfCompressedPixels, uint8_t compressionType);
 	~Header();
 };
